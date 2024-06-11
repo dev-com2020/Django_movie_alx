15e0c651a02431b9d29a28659614bf2d0616a09b
DEV-COM <kwp@tlen.pl>
2024-06-11 15:26:33 +0200

    10:50 15:25

--- moviereviews/accounts/views.py
+++ moviereviews/accounts/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.shortcuts import render, redirect
@@ -24,6 +25,22 @@ def signupaccount(request):
             return render(request, 'signupaccount.html', {'form': UserCreateForm,
                                                           'error': 'Hasła nie pasują do siebie'})
 
+
 def logoutaccount(request):
     logout(request)
     return redirect('home')
+
+
+def loginaccount(request):
+    if request.method == 'GET':
+        return render(request, 'loginaccount.html', {'form': AuthenticationForm})
+    else:
+        user = authenticate(request,
+                            username=request.POST['username'],
+                            password=request.POST['password'])
+        if user is None:
+            return render(request, 'loginaccount.html', {'form': AuthenticationForm(),
+                                                         'error': 'Użytkownik lub hasło nie pasują'})
+        else:
+            login(request, user)
+            return redirect('home')
