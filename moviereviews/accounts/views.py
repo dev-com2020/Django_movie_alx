6c3536f8cf0099ca332bf106cee5b1e0c851997d
DEV-COM <kwp@tlen.pl>
2024-06-11 14:39:06 +0200

    10:50 14:38

--- moviereviews/accounts/views.py
+++ moviereviews/accounts/views.py
@@ -1,19 +1,25 @@
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.shortcuts import render, redirect
+
+from .forms import UserCreateForm
 
 
 def signupaccount(request):
     if request.method == 'GET':
-        return render(request, 'signupaccount.html', {'form': UserCreationForm})
+        return render(request, 'signupaccount.html', {'form': UserCreateForm})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(request.POST['username'],
+            try:
+                user = User.objects.create_user(request.POST['username'],
                                             password=request.POST['password1'])
-            user.save()
-            login(request, user)
-            return redirect('home')
+                user.save()
+                login(request, user)
+                return redirect('home')
+            except IntegrityError:
+                return render(request, 'signupaccount.html', {'form': UserCreateForm,
+                                                              'error': 'Użytkownik już istnieje, wprowadź inną nazwę'})
         else:
-            return render(request, 'signupaccount.html', {'form': UserCreationForm,
+            return render(request, 'signupaccount.html', {'form': UserCreateForm,
                                                           'error': 'Hasła nie pasują do siebie'})
