018e22e158e5164a3429e826cf240628ec9d3b61
DEV-COM <kwp@tlen.pl>
2024-06-11 13:50:44 +0200

    10:50 14:00

--- moviereviews/accounts/views.py
+++ moviereviews/accounts/views.py
@@ -1,6 +1,16 @@
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
 
 def signupaccount(request):
-    return render(request, 'signupaccount.html', {'form': UserCreationForm})
+    if request.method == 'GET':
+        return render(request, 'signupaccount.html', {'form': UserCreationForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(request.POST['username'],
+                                            password=request.POST['password1'])
+            user.save()
+            login(request, user)
+            return redirect('home')
